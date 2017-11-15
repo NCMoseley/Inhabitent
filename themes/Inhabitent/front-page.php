950ea2760124b082fed27680bbf8c1f90cdd6a54
NCMoseley <ncmoseley@gmail.com>
2017-11-15 14:55:31 -0800

    Add blog posts to front page

--- themes/Inhabitent/front-page.php
+++ themes/Inhabitent/front-page.php
@@ -6,7 +6,7 @@
  */
 
 get_header(); ?>
-
+<!-- Hero -->
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
          
@@ -23,23 +23,60 @@ get_header(); ?>
               <img src="wp-content/themes/Inhabitent/images/logos/inhabitent-logo-full.svg" class="logo" alt="full logo">
               </section>
 
-
-            
+<!-- Shop Stuff -->
+            <section class="product-info-container">
+            <h2>Shop Stuff</h2>
             <?php
-               $args = array( 'post_type' => 'post', 'order' => 'ASC' );
+               $terms = get_terms( array(
+                   'taxonomy' => 'product-type',
+                   'hide_empty' => 0,
+               ) );
+
+			   d($terms);
+               if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
+            ?>
+               <div class="product-type-blocks">
+
+                  <?php foreach ( $terms as $term ) : ?>
+
+                     <div class="product-type-block-wrapper">
+                        <img src="<?php echo get_template_directory_uri() . '/images/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+                        <p><?php echo $term->description; ?></p>
+                        <p><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
+                     </div>
+
+                  <?php endforeach; ?>
+
+               </div>
+               
+            <?php endif; ?>
+         </section>
+
+<!-- Journal -->            
+            <?php
+               $args = array( 'post_type' => 'post', 'order' => 'DESC', 'posts_per_page' => '3' );
 
                $product_posts = get_posts( $args ); // returns an array of posts
               ?>
+			  
+             <h1 class="habj">Inhabitant Journal</h1>
 
 			<section class="journal">
             <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+			
+			<div class="thumbs">
             <?php the_post_thumbnail(); ?>
+			<p class="jdate"><?php the_date(); ?></p>
+			<?php the_title(); ?>
+			
+            <a id="jbutton" href="<?php the_permalink(); ?>">Read Entry</a>
 			<?php the_posts_navigation(); ?>
-			<?php the_posts_content(); ?>
+			</div>
+
               <?php endforeach; wp_reset_postdata(); ?>
             </section>
 
-
+<!-- Latest Adventures -->
 	        <section class="adventures-section">
                            <h2>Latest Adventures</h2>
 		              <!-- <div class="latest-adventures"></div> -->
