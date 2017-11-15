edefab5f104b5919ef5f6d4e515509d2f0971c27
NCMoseley <ncmoseley@gmail.com>
2017-11-14 16:20:57 -0800

    Attach About Page image

--- themes/Inhabitent/page-templates/about.php
+++ themes/Inhabitent/page-templates/about.php
@@ -10,14 +10,23 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		  <header class="entry-header custom-hero">
+			  <div class="container">
+			<?php the_title( '<h1 class="entry-title">' , '</h1>' ); ?>	
+			</div>  
+			</header>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-
+			
+		  <div class="container">
+           <div class="entry-content">
+              <h2>Our Story</h2>
+                <?php echo CFS()->get('about_our_story'); ?>
+              <h2>Our Team</h2>
+                <?php echo CFS()->get('about_our_team'); ?>
+               </div>
+             </div>
+           </article>
+        </main>  <!--main -->
+    </div> <!-- primary -->
 <?php get_footer(); ?>
